78dca56a9ce116cd3b5c88618a9dc344ca1f70b0
joaopjk <joaopjk48@gmail.com>
2022-06-19 20:27:29 -0300

    Preparando o caso de uso do User store

---
+++ AdonisJs/roleplay-api/test/users/user.spec.ts
@@ -0,0 +1,17 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group("User", () => {
+    test.only('it should create an user', async (assert) => {
+        const userPayload = { email: 'teste@teste.com', username: 'test', password: 'test', avatar: 'urlqualquer' }
+        const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+        assert.exists(body.user, 'User undefined')
+        assert.exists(body.user.id, 'Id undefined')
+        assert.equal(body.user.email, userPayload.email)
+        assert.equal(body.user.username, userPayload.username)
+        assert.equal(body.user.password, userPayload.password)
+        assert.equal(body.user.avatar, userPayload.avatar)
+    })
+})
