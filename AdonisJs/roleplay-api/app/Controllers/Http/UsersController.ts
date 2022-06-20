110b50cc0b077ef09f06c5d381e97ede684d39e3
joaopjk <joaopjk48@gmail.com>
2022-06-19 23:28:23 -0300

    Criando handle de erro

--- AdonisJs/roleplay-api/app/Controllers/Http/UsersController.ts
+++ AdonisJs/roleplay-api/app/Controllers/Http/UsersController.ts
@@ -1,9 +1,13 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 
 export default class UsersController {
     public async store({ request, response }: HttpContextContract) {
         const userPayload = request.only(['email', 'username', 'password', 'avatar'])
+        const userByEmail = await User.findBy('email', userPayload.email)
+        if (userByEmail)
+            throw new BadRequestException('email already in use', 409)
         const user = await User.create(userPayload)
         return response.created({ user })
     }
--- AdonisJs/roleplay-api/test/users/user.spec.ts
+++ AdonisJs/roleplay-api/test/users/user.spec.ts
@@ -1,17 +1,40 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
-test.group("User", () => {
-    test.only('it should create an user', async (assert) => {
+test.group("User", (group) => {
+    test('it should create an user', async (assert) => {
         const userPayload = { email: 'teste@teste.com', username: 'test', password: 'test', avatar: 'urlqualquer' }
         const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
         assert.exists(body.user, 'User undefined')
         assert.exists(body.user.id, 'Id undefined')
         assert.equal(body.user.email, userPayload.email)
         assert.equal(body.user.username, userPayload.username)
-        assert.notExists(body.user.password,'Password defined')
+        assert.notExists(body.user.password, 'Password defined')
         assert.equal(body.user.avatar, userPayload.avatar)
+    })
+
+    test('it should return 409 when email is already in use', async (assert) => {
+        const { email } = await UserFactory.create()
+        const { body } = await supertest(BASE_URL).post('/users').send({
+            email,
+            username: 'test',
+            password: 'test'
+        }).expect(409)
+
+        assert.include(body.message, 'email')
+        assert.equal(body.code, 'BAD_REQUEST')
+        assert.equal(body.status, 409)
+    })
+
+    group.beforeEach(async () => {
+        await Database.beginGlobalTransaction()
+    })
+
+    group.afterEach(async () => {
+        await Database.rollbackGlobalTransaction()
     })
 })
