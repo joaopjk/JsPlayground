455eb6350d77969d16c28af8eeb45fded63264ec
joaopjk <joaopjk48@gmail.com>
2022-06-19 21:31:24 -0300

    Termino do TDD do User create

--- AdonisJs/roleplay-api/app/Models/User.ts
+++ AdonisJs/roleplay-api/app/Models/User.ts
@@ -1,25 +1,32 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public username:string
+  public username: string
 
   @column()
-  public email:string
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
 
   @column()
-  public password:string
-
-  @column()
-  public avatar:string
+  public avatar: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password)
+      user.password = await Hash.make(user.password)
+  }
 }
