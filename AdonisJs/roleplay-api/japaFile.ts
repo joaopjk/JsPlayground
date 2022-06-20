5e89cd207fba127109a039c2c0383c4d895f9f14
joaopjk <joaopjk48@gmail.com>
2022-06-19 21:19:50 -0300

    Configurando migrations nos tests

--- AdonisJs/roleplay-api/japaFile.ts
+++ AdonisJs/roleplay-api/japaFile.ts
@@ -3,6 +3,7 @@ import { join } from 'path'
 import getPort from 'get-port'
 import { configure } from 'japa'
 import sourceMapSupport from 'source-map-support'
+import execa from 'execa'
 
 process.env.NODE_ENV = 'testing'
 process.env.ADONIS_ACE_CWD = join(__dirname)
@@ -14,10 +15,21 @@ async function startHttpServer() {
     await new Ignitor(__dirname).httpServer().start()
 }
 
-/**
- * Configure test runner
- */
+async function runMigrations() {
+    await execa.node('ace', ['migrations:run'], {
+        stdio: 'inherit'
+    })
+}
+
+async function rollbackMigrations() {
+    await execa.node('ace', ['migrations:rollback'], {
+        stdio: 'inherit'
+    })
+}
+
+
 configure({
     files: ['test/**/*.spec.ts'],
-    before: [startHttpServer],
+    before: [runMigrations, startHttpServer],
+    after: [rollbackMigrations]
 })
