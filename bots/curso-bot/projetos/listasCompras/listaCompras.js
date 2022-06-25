144d13653b5c6cb3cb40ecf2e51613e6bab34ef8
joaopjk <joaopjk48@gmail.com>
2022-06-25 20:38:05 -0300

    iniciando projeto da lista de compras

---
+++ bots/curso-bot/projetos/listasCompras/listaCompras.js
@@ -0,0 +1,31 @@
+const env = require('../../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+const bot = new Telegraf(env.token)
+let lista = []
+const botoes = () => Extra.markup(
+    Markup.inlineKeyboard(
+        lista.map(item => Markup.callbackButton(item, `delete ${item}`)),
+        {columns: 3})
+)
+
+bot.start(async ctx => {
+    const name = ctx.update.message.from.first_name
+    await ctx.reply(`Seja bem vindo, ${name}`)
+    await ctx.reply('Escreva os itens que você deseja adicionar...')
+})
+
+bot.on('text', ctx => {
+    const item = ctx.update.message.text
+    lista.push(item)
+    ctx.reply(`${item} foi adicionado!`, botoes())
+})
+
+bot.action(/delete (.+)/, ctx => {
+    lista = lista.filter(i => i !== ctx.match[1])
+    ctx.reply(`${ctx.match[1]} foi deletado!`, botoes())
+})
+
+bot.startPolling()
