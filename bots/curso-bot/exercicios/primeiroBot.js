50b5e62ec3dd3c85d0efbdd1a19eb334b59741c2
joaopjk <joaopjk48@gmail.com>
2022-06-25 15:40:45 -0300

    Primeiro bot

--- bots/curso-bot/exercicios/primeiroBot.js
+++ bots/curso-bot/exercicios/primeiroBot.js
@@ -2,6 +2,7 @@ const env = require('../.env')
 const Telegraf = require('telegraf')
 
 const bot = new Telegraf(env.token)
+
 bot.start(ctx => {
     const from = ctx.update.message.from
     console.log(from)
