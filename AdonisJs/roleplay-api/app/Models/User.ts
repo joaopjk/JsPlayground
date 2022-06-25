940657a6cf7d89213271fa33012211a1d42f8641
joaopjk <joaopjk48@gmail.com>
2022-06-25 12:09:51 -0300

    Configure table linkTokens

--- AdonisJs/roleplay-api/app/Models/User.ts
+++ AdonisJs/roleplay-api/app/Models/User.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
 import Hash from '@ioc:Adonis/Core/Hash'
+import LinkToken from './LinkToken'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -29,4 +30,9 @@ export default class User extends BaseModel {
     if (user.$dirty.password)
       user.password = await Hash.make(user.password)
   }
+
+  @hasMany(() => LinkToken, {
+    foreignKey: 'userId'
+  })
+  public tokens: HasMany<typeof LinkToken>
 }
