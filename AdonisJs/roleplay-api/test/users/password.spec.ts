75e75add5868e66fe9d2e8d9595da57291a41535
joaopjk <joaopjk48@gmail.com>
2022-06-24 22:02:01 -0300

    it should send and email with forgot password instructions with mock return

---
+++ AdonisJs/roleplay-api/test/users/password.spec.ts
@@ -0,0 +1,25 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Password', (group) => {
+
+    test.only('it should send and email with forgot password instructions', async (assert) => {
+        const { email } = await UserFactory.create()
+        await supertest(BASE_URL).post('/forgot-password')
+            .send({
+                email,
+                resetPasswordUrl: 'https://www.google.com/gmail/'
+            }).expect(204)
+    })
+
+    group.beforeEach(async () => {
+        await Database.beginGlobalTransaction()
+    })
+    group.afterEach(async () => {
+        await Database.rollbackGlobalTransaction()
+    })
+})
