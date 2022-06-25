26a94a27b2746ff265ac0dbedaac0b9b2eb0bdc9
joaopjk <joaopjk48@gmail.com>
2022-06-25 16:52:35 -0300

    Devolvendo arquivos recebidos

--- bots/curso-bot/exercicios/devolverArquivo.js
+++ bots/curso-bot/exercicios/devolverArquivo.js
@@ -13,7 +13,7 @@ bot.on('voice', async ctx => {
 bot.on('photo', async ctx => {
     const id = ctx.update.message.photo[0].file_id
     const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
-    ctx.replyWithPhoto({url:`${env.apiFileUrl}/${res.data.result.file_path}`})
+    ctx.replyWithPhoto({url: `${env.apiFileUrl}/${res.data.result.file_path}`})
 })
 
 bot.startPolling()
