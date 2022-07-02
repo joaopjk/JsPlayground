a776d456437905bc63ccb3611f1c3ea27dc70707
joaopjk <joaopjk48@gmail.com>
2022-07-02 18:22:36 -0300

    Configurando o mongoose

--- NestJs/api-smartranking/src/app.module.ts
+++ NestJs/api-smartranking/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    MongooseModule.forRoot(`${process.env.MONGODB_URL}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }),
+    JogadoresModule
+  ],
   controllers: [],
   providers: []
 })
