bfb5a3ab808341a98620894f9f685b7998ace0da
joaopjk <joaopjk48@gmail.com>
2022-07-03 15:26:42 -0300

    Schema e interface de desafios

--- NestJs/api-smartranking/src/desafios/desafios.module.ts
+++ NestJs/api-smartranking/src/desafios/desafios.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DesafiosController } from './desafios.controller';
 import { DesafiosService } from './desafios.service';
+import { DesafioSchema } from './interfaces/desafio.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Desafio', schema: DesafioSchema }])
+  ],
   controllers: [DesafiosController],
   providers: [DesafiosService]
 })
