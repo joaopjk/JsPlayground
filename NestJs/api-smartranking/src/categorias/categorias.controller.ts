6b5074689983d87b0bf2049b436b2c2421d79300
joaopjk <joaopjk48@gmail.com>
2022-07-02 22:13:24 -0300

    Consultar categoria pelo Id

--- NestJs/api-smartranking/src/categorias/categorias.controller.ts
+++ NestJs/api-smartranking/src/categorias/categorias.controller.ts
@@ -1,6 +1,8 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
   UsePipes,
   ValidationPipe
@@ -19,5 +21,17 @@ export class CategoriasController {
     @Body() criarCategoriaDto: CriaCategoriaDto
   ): Promise<Categoria> {
     return await this.categoriasService.criarCategoria(criarCategoriaDto);
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriasService.consultarTodasCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultarCategoriaPeloId(
+    @Param('categoria') categoria: string
+  ): Promise<Categoria> {
+    return this.categoriasService.consultarCategoriaPeloId(categoria);
   }
 }
--- NestJs/api-smartranking/src/categorias/categorias.service.ts
+++ NestJs/api-smartranking/src/categorias/categorias.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CriaCategoriaDto } from './dtos/criar-categoria.dto';
@@ -9,6 +13,21 @@ export class CategoriasService {
   constructor(
     @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>
   ) {}
+
+  async consultarTodasCategorias(): Promise<Categoria[]> {
+    return await this.categoriaModel.find().exec();
+  }
+
+  async consultarCategoriaPeloId(categoria: string): Promise<Categoria> {
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({ categoria })
+      .exec();
+
+    if (!categoriaEncontrada)
+      throw new NotFoundException(`Categora: ${categoria} não encontrada!`);
+
+    return categoriaEncontrada;
+  }
 
   async criarCategoria(
     criarCategoriaDto: CriaCategoriaDto
