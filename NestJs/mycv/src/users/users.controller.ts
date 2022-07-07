7d6a6e28c76fc61d4025d5c43b3fc3efd9635439
joaopjk <joaopjk48@gmail.com>
2022-07-07 20:11:52 -0300

    Refatoração

--- NestJs/mycv/src/users/users.controller.ts
+++ NestJs/mycv/src/users/users.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
@@ -17,27 +18,29 @@ export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.userService.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto) {
+    await this.userService.create(body.email, body.password);
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
   @Get()
-  findAllUsers(@Query('email') email: string) {
-    return this.userService.find(email);
+  async findAllUsers(@Query('email') email: string) {
+    return await this.userService.find(email);
   }
 
   @Delete('/:id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(parseInt(id));
+  async remove(@Param('id') id: string) {
+    return await this.userService.remove(parseInt(id));
   }
 
   @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return this.userService.update(parseInt(id), body);
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return await this.userService.update(parseInt(id), body);
   }
 }
--- NestJs/mycv/src/users/users.service.ts
+++ NestJs/mycv/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -7,23 +7,23 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  create(email: string, password: string) {
+  async create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
-    return this.repo.save(user);
+    return await this.repo.save(user);
   }
 
-  findOne(id: number) {
-    return this.repo.findOneBy({ id });
+  async findOne(id: number) {
+    return await this.repo.findOneBy({ id });
   }
 
-  find(email: string) {
-    return this.repo.findBy({ email });
+  async find(email: string) {
+    return await this.repo.findBy({ email });
   }
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.repo.findOneBy({ id });
-    if (!user) throw Error('User not found!');
+    if (!user) throw new NotFoundException('User not found!');
 
     Object.assign(user, attrs);
 
@@ -32,7 +32,7 @@ export class UsersService {
 
   async remove(id: number) {
     const user = await this.repo.findOneBy({ id });
-    if (!user) throw Error('User not found!');
+    if (!user) throw new NotFoundException('User not found!');
 
     return this.repo.remove(user);
   }
