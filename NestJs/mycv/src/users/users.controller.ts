41c11092a4f63e576608c4c1bd99a98e6e9d5096
joaopjk <joaopjk48@gmail.com>
2022-07-09 11:32:15 -0300

    Use global interceptor

--- NestJs/mycv/src/users/users.controller.ts
+++ NestJs/mycv/src/users/users.controller.ts
@@ -9,7 +9,6 @@ import {
   Post,
   Query,
   Session,
-  UseInterceptors,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
@@ -18,12 +17,10 @@ import { CreateUserDto } from './dtos/create-use.dtos';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private userService: UsersService,
--- NestJs/mycv/src/users/users.module.ts
+++ NestJs/mycv/src/users/users.module.ts
@@ -5,10 +5,15 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+  ],
 })
 export class UsersModule {}
