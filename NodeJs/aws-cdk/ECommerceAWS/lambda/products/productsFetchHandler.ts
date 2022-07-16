e0726761aa80b7a75581a0db7f68fa24de1db3b6
joaopjk <joaopjk48@gmail.com>
2022-07-15 21:54:21 -0300

    Criando a primeira função lambda

--- NodeJs/aws-cdk/ECommerceAWS/lambda/products/productsFetchHandler.ts
+++ NodeJs/aws-cdk/ECommerceAWS/lambda/products/productsFetchHandler.ts
@@ -6,18 +6,28 @@ import {
 
 export async function handler(event: APIGatewayProxyEvent,
   context: Context): Promise<APIGatewayProxyResult> {
+
+  const lambdaRequestId = context.awsRequestId;
+  const gatewayRequestId = event.requestContext.requestId;
   const method = event.httpMethod;
+  
+  console.log(`API Gatway RequestId: ${gatewayRequestId} 
+             - Lambda RequestId: ${lambdaRequestId}`);
+
   if (event.resource === '/products') {
     if (method === 'GET') {
       console.log('GET')
       return {
         statusCode: 200,
         body: JSON.stringify({
+          lambdaRequestId,
+          gateway: gatewayRequestId,
           message: 'Get products - OK'
         })
       }
     }
   }
+
   return {
     statusCode: 400,
     body: JSON.stringify({
